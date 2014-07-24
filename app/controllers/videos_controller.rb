a0a8ae928c4ffcb80bf0b21c7de6dc15a485b1e6
1625081 <jinyongjy@sina.com>
2014-07-24 13:39:20 +0800

    random_video_upload

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -4,6 +4,10 @@ def new
 
 end
 
+def index
+	@videos = Video.all
+end	
+
 def create
 @video = Video.new(video_params)
 @video.save
@@ -14,7 +18,12 @@ def show
 	@video = Video.find(params[:id])
 
 end	
-
+def destroy
+  @video = Video.find(params[:id])
+  @video.destroy
+ 
+  redirect_to videos_path
+end
 private
   def video_params
     params.require(:video).permit(:title,:owner,:youkuid)
