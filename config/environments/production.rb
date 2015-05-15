0b6857a7c02ac887e20f7f8af6cf41ba2ce41ab3
1625081 <jinyongjy@sina.com>
2015-05-15 17:08:06 +0800

    puma.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
@@ -60,7 +60,7 @@ Rails.application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  config.assets.precompile += %w( js-template.js js-load_image.js mediaelementplayer.js mediaelementplayer.css)
+  config.assets.precompile += %w( js-template.js js-load_image.js mediaelementplayer.js mediaelementplayer.css home.js waterfall.js)
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
