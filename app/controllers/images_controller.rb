566ad1708c890bd0a2371af769322c6b8597ad52
1625081 <jinyongjy@sina.com>
2015-06-10 19:06:50 +0800

    fixfix

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -94,10 +94,10 @@ class ImagesController < ApplicationController
   # DELETE /images/1
   # DELETE /images/1.json
   def destroy
-    @image.destroy
+    @image.delete
+    @image.save
     respond_to do |format|
-      #format.html { redirect_to images_url, notice: 'Image was successfully destroyed.' }
-      format.json json: true
+      format.html { redirect_to home_path, notice: 'Image was successfully destroyed.' }
     end
   end
 
